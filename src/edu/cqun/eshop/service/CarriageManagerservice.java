5af2a5bebbb7a1e042136834a29c1e26caf5c9f7
xdxlwx <xdxlwx@gmail.com>
2012-09-03 15:14:37 +0800

    commit

--- src/edu/cqun/eshop/service/CarriageManagerservice.java
+++ src/edu/cqun/eshop/service/CarriageManagerservice.java
@@ -9,44 +9,25 @@ public class CarriageManagerservice implements ICarriageManagerService {
 
 
 	@Override
+
 	public boolean addCommodity(Long userId, Commodity commodity) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
-<<<<<<< HEAD
 	public boolean removeCommodity(Long userId, long commodityId) {
-=======
-	public boolean addCommodity(Long userId, Commodity commodity) {
->>>>>>> d565514c97f50fe9aba3ee71ccf6606d5a00771c
 		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
-<<<<<<< HEAD
 	public boolean removeCommodities(Long userId, List<Long> commodityIds) {
-=======
-	public boolean removeCommodity(Long userId, long commodityId) {
->>>>>>> d565514c97f50fe9aba3ee71ccf6606d5a00771c
 		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
-<<<<<<< HEAD
-	public boolean checkCarriage(Long userId) {
-=======
-	public boolean removeCommodities(Long userId, List<Long> commodityIds) {
->>>>>>> d565514c97f50fe9aba3ee71ccf6606d5a00771c
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-<<<<<<< HEAD
-=======
 	public boolean checkCarriage(Long userId) {
 
 		// TODO Auto-generated method stub
@@ -54,7 +35,6 @@ public class CarriageManagerservice implements ICarriageManagerService {
 	}
 
 	@Override
->>>>>>> d565514c97f50fe9aba3ee71ccf6606d5a00771c
 	public boolean pay(Long userId, List<Long> commodityIds) {
 		// TODO Auto-generated method stub
 		return false;
