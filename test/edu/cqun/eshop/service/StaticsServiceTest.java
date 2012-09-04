0d1a806f8209062a6e9a3cc8c37ac11df6f56415
xdxlwx <xdxlwx@gmail.com>
2012-09-04 12:01:03 +0800

    lwx

--- test/edu/cqun/eshop/service/StaticsServiceTest.java
+++ test/edu/cqun/eshop/service/StaticsServiceTest.java
@@ -5,19 +5,44 @@ import static org.junit.Assert.*;
 import java.sql.Timestamp;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import edu.cqun.eshop.Iservice.IStatisticsService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:applicationContext.xml"})
 public class StaticsServiceTest {
 
-	@SuppressWarnings("deprecation")
+	@Autowired
+	IStatisticsService statisticsService;
+	
+//	@Test
+//	public void test() {
+//		Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+//		int mouthnum = timestamp.getMonth()+1;
+//		int daynum = timestamp.getDate();
+//		String mouthString = mouthnum + "";
+//		String mouthString1 = "0" + mouthString;
+//		System.out.println(mouthString);
+//		System.out.println(mouthString1);
+//		System.out.println(timestamp);
+//		System.out.println(timestamp.getYear()+1900);
+//		System.out.println(timestamp.getDate());
+//		System.out.println(timestamp.getMonth()+1);
+//		System.out.println(timestamp.getDay());
+//	}
+	
 	@Test
-	public void test() {
-		Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-		System.out.println(timestamp);
-		System.out.println(timestamp.getYear()+1900);
-		System.out.println(timestamp.getDate());
-		System.out.println(timestamp.getMonth()+1);
-		System.out.println(timestamp.getDay());
+	public void test1() {
+		double d = statisticsService.getDailyProfit(Timestamp.valueOf("2012-09-03 17:11:25"));
+		System.out.println(d);
 	}
 	
-	
+	@Test
+	public void test2() {
+//		statisticsService.get
+	}
 }
