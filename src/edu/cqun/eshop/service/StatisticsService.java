98fdcbdfee4bdd8eae5bcee02c01f7d578cfba3b
xdxlwx <xdxlwx@gmail.com>
2012-09-04 15:36:54 +0800

    StatisticsService

--- src/edu/cqun/eshop/service/StatisticsService.java
+++ src/edu/cqun/eshop/service/StatisticsService.java
@@ -1,11 +1,14 @@
 package edu.cqun.eshop.service;
 
 import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.opensymphony.xwork2.Result;
 
 import edu.cqun.eshop.Iservice.IStatisticsService;
 import edu.cqun.eshop.dao.ImportListDAO;
@@ -26,7 +29,6 @@ public class StatisticsService implements IStatisticsService{
 	@Autowired
 	private OtherPayDAO otherPayDAO;
 	
-	
 	private String startTime = "00:00:00";
 	private String endTime = "23:59:59";
 	
@@ -38,27 +40,72 @@ public class StatisticsService implements IStatisticsService{
 		String end = dateString + " " + endTime;
 		List<SalesRecord> salesRecords_needed = salesRecordDAO.
 				getOverallSalesByPeriod(Timestamp.valueOf(start), Timestamp.valueOf(end));
-		double total = originProfit(salesRecords_needed);
-		
-		
-		return total;
+		double profit = originProfitCal(salesRecords_needed);
+		List<ImportList> importLists_needed = importListDAO.
+				getOverallImportByPeriod(Timestamp.valueOf(start), Timestamp.valueOf(end));
+		double importCost = importCostCal(importLists_needed);
+		double result = profit - importCost;
+		return result;
 	}
 
 	@Override
 	public double getMonthProfit(Timestamp time) {
 		// TODO Auto-generated method stub
-		
-		return 0;
+		String dateString = getMonth(time);
+		String start = dateString + "-" + "01" + " " + startTime;
+		String end = dateString + "-" + "31" + " " + endTime;
+		return finalProfitCal(start, end);
 	}
 
 	@Override
 	public double getYearProfit(Timestamp time) {
 		// TODO Auto-generated method stub
-		
-		return 0;
+		String dateString = getYear(time);
+		String start = dateString + "-" + "01" + "-" + "01" + " " + startTime;
+		String end = dateString + "-" + "12" + "-" + "31" + " " + endTime;
+		return finalProfitCal(start, end);
 	}
 
-	public double originProfit(List<SalesRecord> list){
+	@Override
+	public double getPeriodProfit(Timestamp start, Timestamp end) {
+		// TODO Auto-generated method stub
+		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+		String startString = df.format(start)+ " " +startTime;
+		String endString = df.format(end)+ " " +endTime;
+		return finalProfitCal(startString, endString);
+	}
+	
+	public double finalProfitCal(String start,String end){
+		List<SalesRecord> salesRecords_needed = salesRecordDAO.
+				getOverallSalesByPeriod(Timestamp.valueOf(start), Timestamp.valueOf(end));
+		double profit = originProfitCal(salesRecords_needed);
+		List<ImportList> importLists_needed = importListDAO.
+				getOverallImportByPeriod(Timestamp.valueOf(start), Timestamp.valueOf(end));
+		double importCost = importCostCal(importLists_needed);
+		List<OtherPay> otherPays_needed = otherPayDAO.
+				getOverallOtherPayByPeriod(Timestamp.valueOf(start), Timestamp.valueOf(end));
+		double otherCost = otherPayCal(otherPays_needed);
+		double result = profit - importCost - otherCost;
+		return result;
+	}
+	
+	public double otherPayCal(List<OtherPay> list){
+		double otherCost = 0;
+		for(OtherPay otherPay: list){
+			otherCost += otherPay.getAmount();
+		}
+		return otherCost;
+	}
+	
+	public double importCostCal(List<ImportList> list){
+		double importCost = 0;
+		for(ImportList importList: list){
+			importCost += importList.getPrice()*importList.getQuantity();
+		}
+		return importCost;
+	}
+	
+	public double originProfitCal(List<SalesRecord> list){
 		double total = 0;
 		for(SalesRecord salesRecord: list){
 			total += salesRecord.getAmount();
@@ -82,4 +129,26 @@ public class StatisticsService implements IStatisticsService{
 		String resultString = year + "-" + mouth + "-" + day;
 		return resultString;
 	}
+	
+	public String getMonth(Timestamp time){
+		int yearnum = time.getYear()+1900;
+		int mouthnum = time.getMonth()+1;
+		String year = yearnum + "";
+		String mouth = mouthnum + "";
+		if(mouthnum<10){
+			mouth = "0"+mouth;
+		}
+		String resultString = year + "-" + mouth;
+		return resultString;
+	}
+	
+	public String getYear(Timestamp time){
+		int yearnum = time.getYear()+1900;
+		String year = yearnum + "";
+		String resultString = year;
+		return resultString;
+	}
+
+	
+	
 }
