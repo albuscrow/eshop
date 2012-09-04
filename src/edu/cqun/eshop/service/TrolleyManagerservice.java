31a17f970c0be0ed75dafb1d42306d01310b7c64
tanxiao <123@123.com>
2012-09-04 11:36:48 +0800

    Signed-off-by: tanxiao <123@123.com>trolley

--- src/edu/cqun/eshop/service/TrolleyManagerservice.java
+++ src/edu/cqun/eshop/service/TrolleyManagerservice.java
@@ -1,5 +1,7 @@
 package edu.cqun.eshop.service;
 
+import java.sql.Timestamp;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,18 +9,34 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import edu.cqun.eshop.Iservice.ITrolleyManagerService;
+import edu.cqun.eshop.dao.BuyerDAO;
 import edu.cqun.eshop.dao.OrderListDAO;
 import edu.cqun.eshop.domain.Commodity;
+import edu.cqun.eshop.domain.OrderList;
 
 @Transactional
 @Service("TrolleyManagerservice")
 public class TrolleyManagerservice implements ITrolleyManagerService {
+	
 	@Autowired
 	private OrderListDAO orderDAO;
+	@Autowired
+	private BuyerDAO buyerDAO;
 	
 	@Override
 	public boolean addCommodity(Long userId, Commodity commodity) {
-		// TODO Auto-generated method stub
+		try {
+			OrderList trolley = new OrderList();
+			trolley.setBuyer(buyerDAO.findById(userId));
+			trolley.setCommodity(commodity);
+			Date now = new Date();
+			Timestamp registerDate = new Timestamp(now.getTime());
+			trolley.setRegisterDate(registerDate);
+			orderDAO.attachDirty(trolley);
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return false;
 	}
 
