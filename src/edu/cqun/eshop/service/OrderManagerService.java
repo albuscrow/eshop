b2a160e051e1b1810b874931959e80c58e553a22
luzheqi <lzq1124@126.com>
2013-09-03 19:23:50 +0800

    addtwoserviceimpl

---
+++ src/edu/cqun/eshop/service/OrderManagerService.java
@@ -0,0 +1,26 @@
+package edu.cqun.eshop.service;
+
+import edu.cqun.eshop.Iservice.IOrderManagerService;
+import edu.cqun.eshop.domain.OrderList;
+
+public class OrderManagerService implements IOrderManagerService {
+
+	@Override
+	public boolean generateOrderList(OrderList orderList) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean findOrderListByUser(long buyerId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean OrderListStatus(long orderId, short OrderStatus) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
