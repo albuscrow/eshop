8c3b95de00c9d4919d17c095832bc7e702567392
luzheqi <lzq1124@126.com>
2013-09-03 21:35:20 +0800

    addallservice

---
+++ src/edu/cqun/eshop/service/CarriageManagerservice.java
@@ -0,0 +1,36 @@
+package edu.cqun.eshop.service;
+
+import java.util.List;
+
+import edu.cqun.eshop.Iservice.ICarriageManagerService;
+import edu.cqun.eshop.domain.Commodity;
+
+public class CarriageManagerservice implements ICarriageManagerService {
+
+	@Override
+	public boolean addCommodity(Commodity commodity) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean removeCommodity(long commodityId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean removeCommodities(List<Long> commodityIds) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean pay(List<Long> commodityIds) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	
+
+}
