1464bdf39f69ce9dbdeb60344b1139a8ddb580b4
luzheqi <lzq1124@126.com>
2013-09-04 10:13:57 +0800

    addservice

---
+++ src/edu/cqun/eshop/service/SystemUserManagerService.java
@@ -0,0 +1,34 @@
+package edu.cqun.eshop.service;
+
+import java.util.List;
+
+import edu.cqun.eshop.Iservice.ISystemUserManagerService;
+import edu.cqun.eshop.domain.User;
+
+public class SystemUserManagerService implements ISystemUserManagerService{
+
+	@Override
+	public boolean addSystemUser(User user) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteSystemUser(long userId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteSystemUsers(List<Long> userIds) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean modifySystemUser(User user) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
