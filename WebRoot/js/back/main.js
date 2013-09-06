b45c7f00fc2e165cbda6b18df98e5c4b75e28afb
luzheqi <lzq1124@126.com>
2013-09-06 10:09:58 +0800

    web

--- WebRoot/js/back/main.js
+++ WebRoot/js/back/main.js
@@ -1,5 +1,14 @@
 $(function init() {
+	//添加菜单栏的那个控件
 	$("#operation").accordion();
+	
+	//添加各个菜单项的相应事件
+	$("#addCommodity").click(function() {
+		$(".content").attr("src","back/addCommodity.action");
+	});
+	
+	$("");
+	
 });
 
 
